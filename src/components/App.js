e71a3b28c2053559edcc6f0aba5ad839e9542726
Billy <billychorey@gmail.com>
2023-09-17 18:38:39 -0400

    react chooks example

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,12 @@ function App() {
     <div className="App">
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <p className="App-intro">
-        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
+        In the React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
+      <ExampleComponent />
       <ExampleComponent />
     </div>
   );
